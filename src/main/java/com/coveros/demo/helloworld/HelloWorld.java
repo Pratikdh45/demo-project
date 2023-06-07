40899da37e1826a95757bf4bf44488eb5d632fbe
Alok Patra <alok.patra@mainline.com>
2023-06-07 11:22:15 +0000

    Update HelloWorld.java

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -2,14 +2,21 @@ package com.coveros.demo.helloworld;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HelloWorld {
 
-  public static void main(final String[] args) {
-    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
-    final LocalDateTime now = LocalDateTime.now();
+    private static final Logger logger = Logger.getLogger(HelloWorld.class.getName());
 
-    System.out.println("Hello, World! The current time is " + dtf.format(now));
-  }
+    public static void main(final String[] args) {
+        // Set the logging level to ALL
+        logger.setLevel(Level.ALL);
 
+        final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+        final LocalDateTime now = LocalDateTime.now();
+
+        // Print log message
+        logger.info("Hello, World! The current time is " + dtf.format(now));
+    }
 }
