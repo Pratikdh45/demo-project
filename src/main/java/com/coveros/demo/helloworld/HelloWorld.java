2046515215ed8b4e41da498eca4f36fe94655941
Alok Patra <alok.patra@mainline.com>
2023-06-08 15:22:32 +0000

    Update HelloWorld.java

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -1,10 +1,15 @@
 package com.coveros.demo.helloworld;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+@Path("/api/hello")
 public class HelloWorld {
 
     private static final Logger logger = Logger.getLogger(HelloWorld.class.getName());
@@ -13,10 +18,16 @@ public class HelloWorld {
         // Set the logging level to ALL
         logger.setLevel(Level.ALL);
 
-        final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+        final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'. '");
         final LocalDateTime now = LocalDateTime.now();
 
         // Print log message
         logger.info("Hello, World! The current time is " + dtf.format(now));
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getUsers() {
+        return "Hello World";
     }
 }
