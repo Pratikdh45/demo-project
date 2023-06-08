037c18a7cd2d7a4e48e392770fcb7dafae0860b0
Alok Patra <alok.patra@mainline.com>
2023-06-08 18:05:39 +0000

    Update HelloWorld.java

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -1,44 +1,33 @@
 package com.coveros.demo.helloworld;
 
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import java.net.InetSocketAddress;
 
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        connectToServer();
+    public static void main(String[] args) throws IOException {
+        int port = 8080; // Change the port number if needed
+        HttpServer server = HttpServer.create();
+        server.bind(new InetSocketAddress(port), 0);
+
+        server.createContext("/", new MyHandler());
+        server.start();
+
+        System.out.println("Server started on port " + port);
     }
 
-    public static void connectToServer() {
-        //Try connect to the server on an unused port eg 9991. A successful connection will return a socket
-        try(ServerSocket serverSocket = new ServerSocket(80)) {
-            Socket connectionSocket = serverSocket.accept();
-
-            //Create Input&Outputstreams for the connection
-            InputStream inputToServer = connectionSocket.getInputStream();
-            OutputStream outputFromServer = connectionSocket.getOutputStream();
-
-            Scanner scanner = new Scanner(inputToServer, "UTF-8");
-            PrintWriter serverPrintOut = new PrintWriter(new OutputStreamWriter(outputFromServer, "UTF-8"), true);
-
-            serverPrintOut.println("Hello World! Enter Peace to exit.");
-
-            //Have the server take input from the client and echo it back
-            //This should be placed in a loop that listens for a terminator text e.g. bye
-            boolean done = false;
-
-            while(!done && scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                serverPrintOut.println("Echo from <Your Name Here> Server: " + line);
-
-                if(line.toLowerCase().trim().equals("peace")) {
-                    done = true;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello, World!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream outputStream = exchange.getResponseBody();
+            outputStream.write(response.getBytes());
+            outputStream.close();
         }
     }
 }
